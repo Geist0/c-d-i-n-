4f259c3b7827e2fa2c21d7c70ad8216a7215af3e
Geist0 <45640222+Geist0@users.noreply.github.com>
2018-12-14 11:14:11 +0100

    carte

---
+++ carte/test.js
@@ -0,0 +1,38 @@
+
+// On initialise la latitude et la longitude de Paris (centre de la carte)
+var lat = 48.852969;
+var lon = 2.349903;
+var macarte = null;
+
+var villes = {
+	"Paris": { "lat": 48.852969, "lon": 2.349903 },
+	"Brest": { "lat": 48.383, "lon": -4.500 },
+	"Quimper": { "lat": 48.000, "lon": -4.100 },
+	"Bayonne": { "lat": 43.500, "lon": -1.467 }
+};
+
+// Fonction d'initialisation de la carte
+function initMap() {
+  var markers = [];
+  // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+          macarte = L.map('map').setView([lat, lon], 11);
+          // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+          L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+              // Il est toujours bien de laisser le lien vers la source des données
+              attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+              minZoom: 1,
+              maxZoom: 20
+          }).addTo(macarte);
+          for (ville in villes) {
+            var marker = L.marker([villes[ville].lat, villes[ville].lon]).addTo(macarte);
+            marker.bindPopup(ville);
+            markers.push(marker);
+          }
+          var group = new L.featureGroup(markers); // Nous créons le groupe des marqueurs pour adapter le zoom
+          macarte.fitBounds(group.getBounds().pad(0.5)); // Nous demandons à ce que tous les marqueurs soient visibles, et ajoutons un padding (pad(0.5)) pour que les marqueurs ne soient pas coupés               	
+      }
+window.onload = function(){
+  // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+  initMap(); 
+};
+               	
